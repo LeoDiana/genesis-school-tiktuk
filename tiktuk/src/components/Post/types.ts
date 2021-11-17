26a5327d540c6ad6295788156f18cfb8e34192ac
LeoDiana <dianaliviz2001@gmail.com>
2021-11-17 20:55:50 +0200

    types for post

--- tiktuk/src/components/Post/types.ts
+++ tiktuk/src/components/Post/types.ts
@@ -8,5 +8,10 @@ export interface PostProp {
     diggCount: number;
     commentCount: number;
     hashtags: Hashtag[];
+    playing: boolean;
+    // eslint-disable-next-line no-unused-vars
+    onClick: (videoId: string)=>void;
+    // eslint-disable-next-line no-unused-vars
+    inView: (videoId: string)=>void;
     [others: string]: any;
 }
