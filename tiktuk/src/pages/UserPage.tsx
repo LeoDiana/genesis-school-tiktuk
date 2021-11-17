c1a74316df6d0856074075c81ec754fdee7e1e2f
LeoDiana <dianaliviz2001@gmail.com>
2021-11-17 20:55:34 +0200

    UserPage styles

--- tiktuk/src/pages/UserPage.tsx
+++ tiktuk/src/pages/UserPage.tsx
@@ -1,3 +1,4 @@
+import { Grid } from '@mui/material';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import UserFeed from '../components/User/UserFeed';
@@ -9,10 +10,14 @@ const UserPage = () => {
   const { username } = useParams();
 
   return (
-    <div>
-      <UserInfo username={username || DEFAULT_USERNAME} />
-      <UserFeed username={username || DEFAULT_USERNAME} />
-    </div>
+    <Grid container direction="column" alignContent="center" spacing={2}>
+      <Grid item>
+        <UserInfo username={username || DEFAULT_USERNAME} />
+      </Grid>
+      <Grid item>
+        <UserFeed username={username || DEFAULT_USERNAME} />
+      </Grid>
+    </Grid>
   );
 };
 export default UserPage;
